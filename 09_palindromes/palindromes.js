2c67584fe8dc4577cfb82e995b9ee5187591eeb8
Toby Denton <139702536+Denton95@users.noreply.github.com>
2023-10-12 15:06:26 +0100

    palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let stringClean = string.toLowerCase().replace(/\W/g, '');
+    let reverseStringClean = stringClean.split('').reverse().join('');
 
+    return stringClean === reverseStringClean;
 };
 
 // Do not edit below this line
