b7bb5fa1c58e68e9353ed387f4f5162576f3a8dc
Toby Denton <139702536+Denton95@users.noreply.github.com>
2023-10-12 17:07:19 +0100

    fibonacci sequence v diff solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 1)return "OOPS";
+    if (x === 0 ) return 0;
+    let array = [];
+    let value = 0;
+    let newValue = [];
+    for(i = 0 ; i < x ; i++){
+        if(i<2){
+           array[i] = 1;
+        }
+        else
+        {
+        newValue = array.slice(i-2, i);
+        value = newValue.reduce((total, currentValue)=> {
+            return (total + currentValue);
+        },0);
+        array[i] = value;
+        };
+    };
+    return array[x-1];
 };
 
 // Do not edit below this line
